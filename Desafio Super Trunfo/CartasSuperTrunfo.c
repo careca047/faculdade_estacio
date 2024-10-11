ccc8a3358afcf847932fa8d769ff49c9878e64e7
careca047 <kaina.vieira@hotmail.com>
2024-10-11 20:20:03 -0300

    inserção dos scanfs, e alteração nos cometários

--- Desafio Super Trunfo/CartasSuperTrunfo.c
+++ Desafio Super Trunfo/CartasSuperTrunfo.c
@@ -10,10 +10,13 @@ int main() {
     int populacao, pontos_tur;
     float area, pib;
     //optei por criar as variaveis por tipo, conforme mencionado no vídeo de variaveis pelo professor Sergio Cardoso
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    scanf ("Insira o nome da cidade: \n");
+    scanf ("Insira o código da cidade: \n");
+    scanf ("Quantidade de habitantes: \n");
+    scanf ("PIB: \n");
+    scanf ("Insira a area da cidade: \n");
+    scanf ("Insira a quantidade de pontos turisticos: \n");
+    //cada scanf vai puxar uma informação para cada variavel
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
